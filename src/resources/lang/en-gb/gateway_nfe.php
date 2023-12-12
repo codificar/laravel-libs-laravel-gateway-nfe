092dc45bf08490a64a8f6bbf30a9820bba1a4cd2
rafael.rehfeld <rafael.rehfeld@codificar.com.br>
2023-12-12 13:45:44 -0300

    corrigindo tradução en-gb

---
+++ src/resources/lang/en-gb/gateway_nfe.php
@@ -0,0 +1,121 @@
+<?php
+return [
+	'required_error' => 'Fill in this field',
+
+	'company' => 'Company',
+	'company_locale' => 'Where is the company located?',
+	'site' => 'Company website',
+	'estate' => 'State',
+	'city' => 'City',
+	'zipcode' => 'zip code',
+	'place' => 'Place',
+	'neighborhood' => 'Neighborhood',
+	'number' => 'Number',
+	'complement' => 'Complement',
+
+	'company_identification' => 'Company identification',
+
+	'social_reason' => 'Company name',
+	'fantasy_name' => 'Fantasy name',
+	'municipal_register' => 'Municipal registration',
+	'estate_register' => 'State Registration',
+	'commercial_phone' => 'Commercial phone',
+	'nacional_optant' => 'Simple national option',
+	'cultural_promter' => 'Cultural Incentivator',
+
+	'update_button' => 'Update',
+	'register_button' => 'Register',
+
+
+	'valid_login' => 'Login already Authenticated',
+	'pendent_login' => 'Login Pending',
+	'acess_data' => 'Access data',
+	'login' => 'Login (CNPJ without punctuation)',
+	'link' => 'Link',
+	'auth_doc' => 'Certificate already Authenticated',
+	'pendent_doc' => 'Certificate Pending',
+	'digital_doc' => 'Digital Certificate',
+	'doc_pfx' => 'Certificate (.pfx or .p12)',
+	'password' => 'Password',
+	'name' => 'Name',
+	'valid_date' => 'Valid until',
+
+	// Issuer
+	"document_company_required" => "Document is required",
+	"document_company_unique" => "Document already registered",
+
+	// NFE List
+	"filter" => "Filter",
+	"filters" => "Filters",
+	"clean_filters" => "Clean Filters",
+	"search" => "Search",
+
+	"issuer" => "Issuer",
+	"user" => "User",
+	"institution" => "Institution",
+
+	"select" => "Select",
+	"emmit_by" => "Issued from",
+	"emmit_to" => "Issued to",
+
+	"start_date" => "Start Date",
+	"btween" => "Until",
+	"end_date" => "End Date",
+
+	"unassigned" => "Unassigned",
+	"status_reason" => "STATUS REASON",
+	"emmit_by_title" => "ISSUED FROM",
+	"emmit_to_title" => "ISSUED TO",
+	"value" => "VALUE",
+	"emission_date" => "ISSUE DATE",
+	"view_pdf" => "View PDF",
+
+	// Settings
+	"nfe_settings" => "Invoice Settings",
+	"api_key" => "API key",
+	"enotas_settings" => "Invoice Settings",
+	"webhook_rote" => "Route to webhook",
+	"enable" => "Enabled",
+	"yes" => "yes",
+	"no" => "No",
+	"webhook_key" => "Key to the weebhook",
+	"generate_key" => "Generate Key",
+	"mail_copy" => "Email for copy of notes",
+	"enotas_env" => "eNotas environment",
+	"prod" => "Production",
+	"hom" => "Homologation",
+
+	"settings_title" => "Configurações",
+	"address_title" => "Straighten",
+	"save_settings" => "Configurações Salvas",
+
+	'address_title_required' => 'Preset Straighten',
+	'social_reason_required' => 'Preencha Razão social',
+	'fantasy_name_required' => 'Preencha Nome fantasia',
+	'municipal_register_required' => 'Preencha Encrição municipal',
+	'estate_register_required' => 'Preencha Encrição Estadual',
+	'commercial_phone_required' => 'Preencha Telefone comercial',
+	'nacional_optant_required' => 'Preencha Optante simple national',
+	'cultural_promter_required' => 'Preencha Cultural Incentivator',
+
+	'provider_id_required' => 'Preset provider ID',
+	'ibge_code_not_found' => 'IBGE ID not found',
+
+	"address_required" => "Endereço é obrigatório",
+	"address_city_required" => "Cidade é obrigatória",
+	"address_country_required" => "Country é obrigatório",
+	"address_number_required" => "Number and obrigatório",
+	"address_number_numeric" => "Or number field only oil numeric values",
+	"address_state_required" => "Estado é obrigatória",
+	"address_neighbour_required" => "Bairro é obrigatório",
+
+	"note_description" => "Descrição do serviço na nota",
+	"motoboy_nfe_emmit_date" => "Day of the month of the issuance of NFE two Motoboys",
+	"system_nfe_emmit_date" => "Day of the month of issuance of NFE do Sistema",
+
+	"note_description_title" => "Text that will be used as a description of the note service",
+	"motoboy_nfe_emmit_date_title" => "Day no more than the issuance of two Motoboy notes",
+	"system_nfe_emmit_date_title" => "Day no more than issuance of system notes",
+
+	"link_title" => "Link"
+];
